b3ec09c9e9a679bf0cb896e1cb8e8dd651f135be
Lionel Eyraud-Dubois <lionel.eyraud-dubois@inria.fr>
2025-04-05 19:25:07 +0200

    Display an error message when exporting fails

--- export_window.py
+++ export_window.py
@@ -28,12 +28,23 @@ class ExportWindow(tkinter.Toplevel):
     def update_text(self, *args):
         not_mixed = self.not_mix_var.get()
         if not not_mixed:
-            self.order = ordering.get_mixed_ordering_from_result(self.extracted)
+            try:
+                order = ordering.get_mixed_ordering_from_result(self.extracted)
+            except RuntimeError:
+                order = None
         else:
             include_initial_state = self.include_initial_state_var.get()
-            self.order = ordering.get_ordering_from_result(self.extracted,
+            order = ordering.get_ordering_from_result(self.extracted,
                                                            with_already_present=include_initial_state)
-        self.daftmav_str = daftmav.export_ordering(self.order)
+        if order is None:
+            self.daftmav_str = ("Error! Export failed!\n"
+                                "Did you 'Solve for a system'?\n"
+                                "Please submit an issue\n"
+                                "if you did, with a\n"
+                                "screenshot of the app.")
+            self.text_entry.config(fg="red")
+        else:
+            self.daftmav_str = daftmav.export_ordering(order)
         self.text_entry.delete('1.0', 'end')
         self.text_entry.insert('1.0', self.daftmav_str)
         self.text_entry.see('1.0')
@@ -68,7 +79,7 @@ class ExportWindow(tkinter.Toplevel):
         
         daftmav_frame = ttk.Frame(self, padding=2)
         label = ttk.Label(daftmav_frame, text="Colonization Construction v3 (By DaftMav)", wraplength=100)
-        self.text_entry = ScrolledText(daftmav_frame, width=20, height=5, wrap='none')
+        self.text_entry = ScrolledText(daftmav_frame, width=22, height=5, wrap='none')
         self.daftmav_button = ttk.Button(daftmav_frame, text="To clipboard",
                                          command=self.export, width=8)
         label.pack(padx=5, pady=2, side="left", anchor=ttk.N)
